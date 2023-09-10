ac059f9dc23497c0b9dcef3eb813a4d8ad5b5f9a
Hristijan Kocev <hristijankocev@ymail.com>
2023-09-10 19:07:39 +0200

    [Added] Ticket filtering based on category, status or severity

--- app/Livewire/TicketSearch.php
+++ app/Livewire/TicketSearch.php
@@ -10,19 +10,34 @@ class TicketSearch extends Component
 {
     use WithPagination;
 
-    public $search = '';
+    public string $search = '';
+    public string $status = '';
+    public string $category = '';
+    public string $severity = '';
 
     public function render()
     {
-        $tickets = Ticket::query()
-            ->where('title', 'like', '%' . $this->search . '%')
-            ->paginate(10);
+        $query = Ticket::query()
+            ->where('title', 'like', '%' . $this->search . '%');
+
+        if (!empty($this->status)) {
+            $query->where('status', $this->status);
+        }
+
+        if (!empty($this->category)) {
+            $query->where('category_id', $this->category);
+        }
+
+        if (!empty($this->severity)) {
+            $query->where('severity', $this->severity);
+        }
+
+        $tickets = $query->paginate(10);
 
         // Reset to the first page. More on https://livewire.laravel.com/docs/pagination#resetting-the-page
         $this->resetPage();
 
-        return view('components.livewire.ticket-search', ['tickets' => $tickets,
-            'search' => $this->search]);
+        return view('components.livewire.ticket-search', ['tickets' => $tickets]);
     }
 
 }
--- resources/views/components/livewire/ticket-search.blade.php
+++ resources/views/components/livewire/ticket-search.blade.php
@@ -1,6 +1,49 @@
+@php use Database\Factories\CategoryFactory;use Database\Factories\TicketFactory; @endphp
 <div>
-    <input wire:model.live.debounce.250ms="search" wire:model.live.throttle.150ms type="text" class="form-input w-full
-    mb-4 dark:bg-gray-800 dark:text-gray-100 rounded" placeholder="Search for tickets...">
+    <div class="container px-4 my-2 dark:bg-gray-700 rounded">
+        <p class="block text-sm font-medium text-gray-700 dark:text-gray-300 py-2">Search & Filters</p>
+
+        <label for="title">
+            <input wire:model.live.debounce.250ms="search" wire:model.live.throttle.150ms type="text" class="form-input w-full
+        mt-2 dark:bg-gray-800 dark:text-gray-100 rounded" placeholder="Search by title..." id="title">
+        </label>
+        <div class="mt-2">
+            <label for="status" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Filter by
+                Status</label>
+            <select wire:model.live.debounce.150ms="status" wire:model.live.throttle.150ms name="status" id="status"
+                    class="form-select w-full dark:bg-gray-800 text-gray-700 dark:text-gray-300 rounded">
+                <option value="">All</option>
+                @foreach(TicketFactory::$statuses as $status)
+                    <option value="{{ $status }}">{{ ucfirst($status) }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="mt-2">
+            <label for="category" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Filter by
+                category</label>
+            <select wire:model.live.debounce.150ms="category" wire:model.live.throttle.150ms name="category"
+                    id="category"
+                    class="form-select w-full dark:bg-gray-800 text-gray-700 dark:text-gray-300 rounded">
+                <option value="">All</option>
+                @foreach(CategoryFactory::$categories as $key => $category)
+                    <option value="{{ $key + 1 }}">{{ ucfirst($category) }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="mt-2 pb-2">
+            <label for="severity" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Filter by
+                severity</label>
+            <select wire:model.live.debounce.150ms="severity" wire:model.live.throttle.150ms name="severity"
+                    id="severity"
+                    class="form-select w-full dark:bg-gray-800 text-gray-700 dark:text-gray-300 rounded">
+                <option value="">All</option>
+                @foreach(TicketFactory::$severities as $severity)
+                    <option value="{{ $severity }}">{{ ucfirst($severity) }}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+
     @if ($tickets->count() > 0)
         <div>
             @foreach ($tickets as $ticket)
